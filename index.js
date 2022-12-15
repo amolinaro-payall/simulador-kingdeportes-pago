0f5df111e9e33b3d2ff46ed819c8eb641c862248
amolinaro <amolinaro@payall.com.ve>
2022-12-15 15:26:07 -0400

    lol

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const puerto = 8071;
 app.use(Express.json());
 const timeout = 30000;
 let nro = 0;
+let envio = '{"method": "payin","transaction_id": "0X1234","user_id": "3536","amount": "10","currency": "USD","notas": "Gracias","date": "2022-07-20 23:36","payin": "3536","hash": "eb6c9fa9ca021704c3ccc27abc42346a","data": "Se ha procesado con Exito","info": "Usuario: mostasa Saldo: $1,149.93","reference": "350522","status": "200"}';
+const datos = JSON.parse(envio);
 app.post("/gecko/api/payall/payin/", (request, response) =>
 {
     console.log("<<<<<Nueva Peticion:>>>>>" + (nro++))
@@ -12,42 +14,10 @@ app.post("/gecko/api/payall/payin/", (request, response) =>
     
     if(request.body.transaction_c2p.destination_bank_id != '0115' && request.body.transaction_c2p.trx_type == 'anulacion')
     {
-        response.status(200).json(
-            {
-                "method": "payin",
-                "transaction_id": "0X1234",
-                "user_id": "3536",
-                "amount": "10",
-                "currency": "USD",
-                "notas": "Gracias",
-                "date": "2022-07-20 23:36",
-                "payin": "3536",
-                "hash": "eb6c9fa9ca021704c3ccc27abc42346a",
-                "data": "Se ha procesado con Exito",
-                "info": "Usuario: mostasa Saldo: $1,149.93",
-                "reference": "350522",
-                "status": "200"
-              }
-        )
+        response.status(200).send(datos)
     }else
     {
-        response.status(200).json(
-            {
-                "method": "payin",
-                "transaction_id": "0X1234",
-                "user_id": "3536",
-                "amount": "10",
-                "currency": "USD",
-                "notas": "Graias",
-                "date": "2022-07-20 23:36",
-                "payin": "3536",
-                "hash": "eb6c9fa9ca021704c3ccc27abc42346a",
-                "data": "Se ha procesado con Exito",
-                "info": "Usuario: mostasa Saldo: $1,149.93",
-                "reference": "350522",
-                "status": "200"
-              }
-        )
+        response.status(200).send(datos)
     }
 })
 app.listen(puerto, () => console.log("Escuchando en el puerto: "+ puerto))
