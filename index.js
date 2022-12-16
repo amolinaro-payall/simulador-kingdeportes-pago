1b5746a3b55d67742c04100222b807d42505fe9b
Jhofren Rojas <jrojas@payall.com.ve>
2022-12-16 10:54:43 -0400

    funciono

--- index.js
+++ index.js
@@ -6,19 +6,12 @@ const timeout = 30000;
 let nro = 0;
 let envio = '{"method": "payin","transaction_id": "0X1234","user_id": "3536","amount": "10","currency": "USD","notas": "Gracias","date": "2022-07-20 23:36","payin": "3536","hash": "eb6c9fa9ca021704c3ccc27abc42346a","data": "Se ha procesado con Exito","info": "Usuario: mostasa Saldo: $1,149.93","reference": "350522","status": "200"}';
 const datos = JSON.parse(envio);
-app.post("/gecko/api/payall/payin/", (request, response) =>
+app.post("/gecko/api/payall/payin/:user/hash/:token", (request, response) =>
 {
     response.setHeader('content-type', 'application/json');
     console.log("<<<<<Nueva Peticion:>>>>>" + (nro++))
     console.log(request.body)
     console.log("<<<<<Fin Peticion>>>>>" + (nro))
-    
-    if(request.body.transaction_c2p.destination_bank_id != '0115' && request.body.transaction_c2p.trx_type == 'anulacion')
-    {
-        response.status(200).json(datos)
-    }else
-    {
-        response.status(200).json(datos)
-    }
+    response.status(200).json(datos)
 })
 app.listen(puerto, () => console.log("Escuchando en el puerto: "+ puerto))
